a16f96d1fb2457b289a6ea338e7aaeda087ac837
Zhang Chao <zchaozhang@outlook.com>
2018-08-29 22:50:24 +0800

    tf_rnn add rnn_dropout

--- tensorflow/tf_rnn.py
+++ tensorflow/tf_rnn.py
@@ -31,6 +31,42 @@ def tf_rnn_single(N0=3, N2=7, N3=11):
     print('tf_rnn_single hidden:: np vs tf: ', hfe_r5(np4, tf4_))
 
 
+def tf_rnn_dropout(N0=3, N2=7, N3=11, input_keep_prob=0.7, output_keep_prob=0.8, state_keep_prob=0.9):
+    np1 = np.random.randn(N0, N2)
+    np2 = np.random.randn(N0, N3)
+    np_kernel = np.random.randn(N2+N3, N3)
+    np_bias = np.random.randn(N3)
+
+
+    with tf.Graph().as_default() as tfG:
+        tf1 = tf.constant(np1)
+        tf2 = tf.constant(np2)
+        rnn0 = tf.nn.rnn_cell.BasicRNNCell(N3, name='rnn0')
+        dropoutcell = tf.nn.rnn_cell.DropoutWrapper(rnn0, input_keep_prob=input_keep_prob,
+                    output_keep_prob=output_keep_prob, state_keep_prob=state_keep_prob)
+        # tf2 = rnn0.zero_state(N0, tf.float32)
+        tf3,tf4 = dropoutcell(tf1, tf2)
+        z1 = {x.name:x for x in rnn0.weights}
+        aop = [tf.assign(z1['rnn0/kernel:0'], np_kernel), tf.assign(z1['rnn0/bias:0'], np_bias)]
+        tf_in_mask = tfG.get_tensor_by_name('dropout/random_uniform:0')
+        tf_state_mask = tfG.get_tensor_by_name('dropout_1/random_uniform:0')
+        tf_out_mask = tfG.get_tensor_by_name('dropout_2/random_uniform:0')
+    with tf.Session(graph=tfG) as sess:
+        _ = sess.run(aop)
+        tmp1 = [tf3,tf4,tf_in_mask,tf_state_mask,tf_out_mask]
+        tf3_,tf4_,tf_in_mask_,tf_state_mask_,tf_out_mask_ = sess.run(tmp1)
+
+    def _dropout(x, kepr, mask):
+        return x/kepr * np.floor(mask+kepr)
+    tmp1 = _dropout(np1, input_keep_prob, tf_in_mask_)
+    tmp2 = hf_tanh(np.matmul(np.concatenate([tmp1,np2], axis=1), np_kernel) + np_bias)
+    np3 = _dropout(tmp2, output_keep_prob, tf_out_mask_)
+    np4 = _dropout(tmp2, state_keep_prob, tf_state_mask_)
+
+    print('tf_rnn_dropout output:: np vs tf: ', hfe_r5(np3, tf3_))
+    print('tf_rnn_dropout hidden:: np vs tf: ', hfe_r5(np4, tf4_))
+
+
 def tf_rnn_sequence(N0=3, N1=5, N2=7, N3=11):
     np1 = np.random.randn(N0, N1, N2)
     np2 = np.random.randint(2, N1, size=[N0])
@@ -317,6 +353,8 @@ def tf_gru_sequence(N0=3, N1=5, N2=7, N3=11):
 
 if __name__=='__main__':
     tf_rnn_single()
+    print('')
+    tf_rnn_dropout()
     print('')
     tf_rnn_sequence()
     print('')
